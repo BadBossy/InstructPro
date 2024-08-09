6ed425ed73f0adf0b8f8f977834fbf67bb344242
Hendrik <badbossy@hotmail.de>
2024-08-09 22:01:22 +0200

    feat: manual renderer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router'
+import { ManualRendererComponent } from './features/manual/manual-renderer/manual-renderer.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: 'manual',
+    component: ManualRendererComponent
+  }
+]
---
+++ src/app/features/manual/manual-renderer/manual-renderer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-renderer',
+  templateUrl: './manual-renderer.component.html',
+  styleUrls: ['./manual-renderer.component.scss'],
+  standalone: true
+})
+export class ManualRendererComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
